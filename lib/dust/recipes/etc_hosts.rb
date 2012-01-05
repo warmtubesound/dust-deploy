b047f897070d4648c5cd51b9465d9c8f1d22972e
kris kechagia <kk@rndsec.net>
2012-01-05 10:56:54 +0100

    pushes /etc/hosts regardless of daemon specified, restarts daemon only if specified

--- lib/dust/recipes/etc_hosts.rb
+++ lib/dust/recipes/etc_hosts.rb
@@ -3,11 +3,13 @@ class EtcHosts < Thor
   def deploy node, daemon, options
     template_path = "./templates/#{ File.basename(__FILE__).chomp( File.extname(__FILE__) ) }"
 
-    return unless node.package_installed?('dnsmasq')
     node.scp("#{template_path}/hosts", '/etc/hosts')
 
     # restart dns service
-    node.restart_service daemon if options.restart?
+    if options.restart? and daemon.is_a? String
+      node.package_installed? daemon
+      node.restart_service daemon if options.restart?
+    end
   end
 end
 
