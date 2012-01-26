af187469ae4cac9995869099eb83b7dacb133f97
kris kechagia <kk@rndsec.net>
2012-01-26 11:57:37 +0100

    locale recipe now installs language-base package of selected language on ubuntu nodes

--- lib/dust/recipes/locale.rb
+++ lib/dust/recipes/locale.rb
@@ -1,6 +1,10 @@
 class Locale < Recipe
   desc 'locale:deploy', 'configures system locale'
   def deploy 
+    # ubuntu needs a proper language pack
+    language = @config.split('_').first
+    @node.install_package "language-pack-#{language}-base" if @node.is_ubuntu?
+    
     if @node.uses_apt?
       ::Dust.print_msg "setting locale to '#{@config}'"
       @node.write '/etc/default/locale', "LANGUAGE=#{@config}\nLANG=#{@config}\nLC_ALL=#{@config}\nLC_CTYPE=#{@config}\n", :quiet => true
