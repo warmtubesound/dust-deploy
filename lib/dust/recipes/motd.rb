b63e8503643d471009be40f00d8ffcc6e712682c
kris kechagia <kk@rndsec.net>
2012-02-07 11:26:27 +0100

    adds motd:status

--- lib/dust/recipes/motd.rb
+++ lib/dust/recipes/motd.rb
@@ -3,4 +3,12 @@ class Motd < Recipe
   def deploy    
     @node.deploy_file "#{@template_path}/motd", '/etc/motd', :binding => binding
   end
+  
+  desc 'motd:status', 'shows current message of the day'
+  def status
+    ::Dust.print_msg 'getting /etc/motd'
+    ret = @node.exec 'cat /etc/motd'
+    ::Dust.print_result ret[:exit_code]
+    ::Dust.print_ret ret
+  end  
 end
