fc752ea2171033064ac2b097efabee073078cece
kris kechagia <kk@rndsec.net>
2012-02-09 13:29:16 +0100

    if yes/true/enabled is specified only for a recipe, continue using config = {}, so defaults will be used

--- lib/dust/recipe.rb
+++ lib/dust/recipe.rb
@@ -6,8 +6,15 @@ class Recipe < Thor
     # prepare class variables
     @template_path = "./templates/#{recipe}"
     @node = node
-    @config = config
     @options = options
+
+    # if this recipe just was defined as true, yes or 'enabled', 
+    # continue with empty @config, so defaults get used
+    if config.is_a? TrueClass or config == 'enabled'
+      @config = {}
+    else
+      @config = config
+    end
 
     # run task
     send context
