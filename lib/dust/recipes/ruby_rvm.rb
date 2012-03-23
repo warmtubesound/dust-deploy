9adb3ffd2686a6a0ba10ba4cd7e6dc9f4b226f95
kris kechagia <kk@rndsec.net>
2012-03-23 14:39:22 +0100

    huge refactoring of ruby_rvm recipe

--- lib/dust/recipes/ruby_rvm.rb
+++ lib/dust/recipes/ruby_rvm.rb
@@ -1,45 +1,73 @@
 class RubyRvm < Recipe
-  desc 'ruby_rvm:deploy', 'installs and sets specified ruby version using rvm'
+  desc 'ruby_rvm:deploy', 'installs rvm and ruby for a user'
   def deploy
-    return ::Dust.failed 'os currently not supported :/' unless @node.is_ubuntu?
+    # TODO: rvm only works if your user uses bash/zsh as login shell, check
+    
+    # dependency needed by rvm
+    # TODO: these are only debian/ubuntu dependencies, add for other distributions as well
+    if @node.uses_apt?
+      return unless @node.install_package 'bash'
+      return unless @node.install_package 'curl'
+      return unless @node.install_package 'dh-autoreconf'
+      @node.install_package 'libxml2-dev'
+      @node.install_package 'libxslt1-dev'
+      @node.install_package 'libreadline6-dev'
+      @node.install_package 'zlib1g-dev'
+    end
 
-    return unless @node.install_package 'ruby-rvm'
-    return unless @node.install_package 'libyaml-dev' # needed for yaml output, otherwise gem complains
+    @config.each do |user, version|
+      unless @node.user_exists? user, :quiet => true
+        ::Dust.print_warning "user #{user} doesn't exist. skipping"
+        next
+      end
 
-    return unless install_ruby @config
-    return unless set_default @config 
+      return unless install_rvm user
+      return unless install_ruby user, version
+      return unless set_default user, version
+    end
   end
   
   desc 'ruby_rvm:status', 'shows current ruby version'
   def status
-    ::Dust.print_msg 'getting current ruby-version'
-    ret = @node.exec 'rvm use'
-    ::Dust.print_result ret[:exit_code]
-    ::Dust.print_ret ret
+    @config.each do |user, version|
+      ::Dust.print_msg "getting current ruby-version for user #{user}"
+      ret = @node.exec 'rvm use', :as_user => user
+      ::Dust.print_result ret[:exit_code]
+      ::Dust.print_ret ret
+    end
   end
 
 
   private
 
-  def install_ruby version
-    return true if installed? version
-    ::Dust.print_msg "downloading, compiling and installing ruby-#{version}\n"
-    ret = @node.exec "rvm install ruby-#{version}", :live => true
-    return ::Dust.print_failed 'error installing ruby' unless ret[:exit_code] == 0
-    true
+  def install_rvm user
+    # check if rvm is already installed
+    if @node.exec('which rvm', :as_user => user)[:exit_code] == 0
+      ::Dust.print_msg "updating rvm for user #{user}"
+      return ::Dust.print_result @node.exec('rvm get latest', :as_user => user)[:exit_code]
+
+    else
+      ::Dust.print_msg "installing rvm for user #{user}"
+      return ::Dust.print_result @node.exec("curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer |bash -s stable",
+                                            :as_user => user)[:exit_code]
+    end
   end
 
-  def set_default version
-    ::Dust.print_msg "setting ruby-#{version} as system default\n"
-    ret = @node.exec "rvm use ruby-#{version} --default", :live => true
-    return ::Dust.print_failed "error setting default ruby version"  unless ret[:exit_code] == 0
-    true
+  def install_ruby user, version
+    return true if installed? user, version
+    ::Dust.print_msg "downloading, compiling and installing ruby-#{version}"
+    ::Dust.print_result  @node.exec("rvm install ruby-#{version}", :as_user => user)[:exit_code]
   end
 
-  def installed? version
-    ret = @node.exec "rvm list |grep ruby-#{version}"
+  def set_default user, version
+    ::Dust.print_msg "setting ruby-#{version} as default"
+    ::Dust.print_result @node.exec("rvm use ruby-#{version} --default", :as_user => user)[:exit_code]
+  end
+
+  def installed? user, version
+    ret = @node.exec "rvm list |grep ruby-#{version}", :as_user => user
     if ret[:exit_code] == 0
-      return ::Dust.print_ok "ruby-#{version} already installed"
+      return ::Dust.print_ok "ruby-#{version} for user #{user} already installed"
     end
     false
   end
