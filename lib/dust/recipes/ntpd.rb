930f9d306d09ca140b860007829d4bd2e1934b87
kris kechagia <kk@rndsec.net>
2012-04-18 13:27:50 +0200

    ntpd recipe prints warning if openntpd or chrony is detected

--- lib/dust/recipes/ntpd.rb
+++ lib/dust/recipes/ntpd.rb
@@ -1,6 +1,13 @@
 class Ntpd < Recipe
   desc 'ntpd:deploy', 'installs and configures ntpd'
   def deploy
+    # warn if other ntp package is installed
+    [ 'openntpd', 'chrony' ].each do |package|
+      if @node.package_installed? package, :quiet => true
+        ::Dust.print_warning "#{package} installed, might conflict with ntpd, might be deleted" 
+      end
+    end
+
     @node.install_package 'ntp'
     
     service = @node.uses_apt? ? 'ntp' : 'ntpd'
