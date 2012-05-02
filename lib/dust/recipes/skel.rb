c2e74c5a72fb09a8e1b84d6ff7a63d0a465b1605
kris kechagia <kk@rndsec.net>
2012-05-03 00:35:25 +0200

    removes basic_setup in favor of skel recipe

---
+++ lib/dust/recipes/skel.rb
@@ -0,0 +1,13 @@
+class Skel < Recipe
+  desc 'skel:deploy', 'copy default configuration files to users home directory'
+  def deploy 
+    @config.to_array.each do |user|
+      ::Dust.print_msg "deploying homedir skeleton for #{user}\n"
+      Dir["#{@template_path}/.*"].each do |file|
+        next unless File.file? file
+        @node.deploy_file file, "/#{@node.get_home user}/#{File.basename file}", { :binding => binding, :indent => 2 }
+      end
+      puts
+    end
+  end
+end
