e7e46966eeef3947f9d4aadefb29aa6171b43f36
kris kechagia <kk@rndsec.net>
2012-07-30 14:17:16 +0200

    sysctl now accepts name keys in config
    also introduces remove_other_rules() method to remove unmanaged sysctls, not used for now though, since many systems install a lot of sysctl files

--- lib/dust/recipes/sysctl.rb
+++ lib/dust/recipes/sysctl.rb
@@ -2,13 +2,13 @@ class Sysctl < Recipe
   desc 'sysctl:deploy', 'configures sysctl'
   def deploy
     # we need support for /etc/sysctl.d/
-    unless @node.dir_exists? '/etc/sysctl.d/'
+    unless @node.dir_exists?('/etc/sysctl.d/')
       return @node.messages.add('sysctl configuration not supported for your linux distribution').warning
     end
 
     # seperate templates from sysctls
     sysctls = @config.clone
-    templates = sysctls.delete 'templates'
+    templates = sysctls.delete('templates')
 
     # apply template sysctls
     if templates
@@ -18,19 +18,26 @@ class Sysctl < Recipe
       end
     end
 
-    # apply plain sysctls
-    @node.messages.add("configuring plain sysctls\n")
-    apply 'dust', sysctls
+    # apply sysctls
+    sysctls.each do |name, keys|
+      @node.messages.add("configuring sysctls (#{name})\n")
+      apply(name, keys)
+    end
+
+    # don't remove other rules for now
+    # remove_other_rules(sysctls.keys)
   end
 
 
   private
 
-  def apply name, sysctl
+  def apply(name, sysctl)
     sysctl_conf = ''
     sysctl.each do |key, value|
-      msg = @node.messages.add("setting #{key} = #{value}", :indent => 2)
-      msg.parse_result(@node.exec("sysctl -w #{key}=#{value}")[:exit_code])
+      if options.restart?
+        msg = @node.messages.add("setting #{key} = #{value}", :indent => 2)
+        msg.parse_result(@node.exec("sysctl -w #{key}=#{value}")[:exit_code])
+      end
       sysctl_conf << "#{key} = #{value}\n"
     end
 
@@ -38,12 +45,27 @@ class Sysctl < Recipe
     msg.parse_result(@node.write("/etc/sysctl.d/#{name}.conf", sysctl_conf, :quiet => true))
   end
 
+  # removes rules from /etc/sysctl.d/ that are not present in "names"
+  def remove_other(names)
+    @node.messages.add("deleting old rules\n")
+    ret = @node.exec('ls /etc/sysctl.d/* |cat')
+    if ret[:exit_code] != 0
+      return @node.messages.add('couldn\'t get list of files in /etc/sysctl.d, skipping deletion of old rules').warning
+    end
+
+    # delete file if not in config
+    ret[:stdout].each_line do |file|
+      file.chomp!
+      @node.rm(file, :indent => 2) unless names.include?(File.basename(file))
+    end
+  end
+
 
   ### templates ###
 
   # disable allocation of more ram than actually there for postgres
   def postgres
-    database.merge 'vm.overcommit_memory' => 2
+    database.merge('vm.overcommit_memory' => 2)
   end
 
   # redis complains if vm.overcommit_memory != 1
@@ -58,7 +80,7 @@ class Sysctl < Recipe
   # use half of the system memory for shmmax
   # and set shmall according to pagesize
   def database
-    @node.collect_facts :quiet => true
+    @node.collect_facts(:quiet => true)
 
     # get pagesize
     pagesize = @node.exec('getconf PAGESIZE')[:stdout].to_i || 4096
